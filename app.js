b7a192c29108772673568f1626c7961050eb8a05
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-26 17:33:31 +0200

    Pok-dex

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ function displayPokémon(){
     const newPokédexPage = pokédex.pokémonArray;
     for (let i = 0; i < newPokédexPage.length; i++) {
         const pokémon = newPokédexPage[i];
-        pokémonBox.innerHTML += `<span>${pokémon.name}</span><br>`;
+        pokémonBox.innerHTML += `<span><a class='pokémon-name' href='${pokémon.url}'>${pokémon.name[0].toUpperCase()+pokémon.name.slice(1)}</a></span><br>`;
     };
 };
