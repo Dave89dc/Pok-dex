912af113fbe0848caf5b9dd6da3f3aa686943854
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-27 12:56:00 +0200

    pok-dex

--- Model/pokémon.js
+++ Model/pokémon.js
@@ -1,7 +1,22 @@
 class Pokémon{
 
-    constructor(name){
+    constructor(name, stats = []){
         this.name = name;
+        this.stats = stats;
+    };
+
+    addStats(name, baseValue){
+        const newStats = new Stats(name, baseValue);
+        this.stats.push(newStats);
+    }
+
+};
+
+class Stats{
+
+    constructor(name, baseValue){
+        this.name = name;
+        this.baseValue = baseValue;
     };
 
 };
